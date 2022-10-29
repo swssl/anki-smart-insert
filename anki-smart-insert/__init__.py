8db10fd3da14098ada93bce852204dd83f7cab58
swssl <81415137+swssl@users.noreply.github.com>
2022-10-29 10:51:09 +0200

    Add hooked function that is able to process inserted data

--- anki-smart-insert/__init__.py
+++ anki-smart-insert/__init__.py
@@ -1,23 +1,32 @@
 # import the main window object (mw) from aqt
-from aqt import mw
+from aqt import mw, gui_hooks
 # import the "show info" tool from utils.py
 from aqt.utils import showInfo, qconnect
 # import all of the Qt GUI library
 from aqt.qt import *
+from aqt.editor import EditorWebView
 
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
 
-def testFunction() -> None:
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    cardCount = mw.col.cardCount()
-    # show a message box
-    showInfo("Card count: %d" % cardCount)
+def process_inserted_data(mime: QMimeData, editor_web_view: EditorWebView, internal: bool, extended: bool, drop_event: bool,):
+    """Function that is called before something is pasted into any editor field
 
-# create a new menu item, "test"
-action = QAction("test", mw)
-# set it to call testFunction when it's clicked
-qconnect(action.triggered, testFunction)
-# and add it to the tools menu
-mw.form.menuTools.addAction(action)
+    Args:
+        mime (QMimeData): Data to be inserted
+        editor_web_view (EditorWebView): Some kind of editor instance
+        internal (bool): true if the content is copied from inside anki
+        extended (bool): ???
+        drop_event (bool): True if it was a drag-and-drop event instead of ordinary ctrl+v???
+
+    Returns:
+        QMimeData: Edited clipboard data
+    """
+    # Test if data update should be applied
+    if internal or not mime.hasText():
+        return mime
+    data = mime.text()
+    result = QMimeData()
+
+    result.setData("text/plain", QByteArray(f"Insert {data}".encode()))
+    return result
+
+gui_hooks.editor_will_process_mime.append(process_inserted_data)
